7692227ebb7eff6b25274b72fd4573a832bdb5c2
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-02-29 22:40:46 +0500

    firat commit

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина -  HOME',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': True
+    }
+
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
