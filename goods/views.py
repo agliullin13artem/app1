6af68c8b2a1aeb968ce71bf4febc4ad8a3e96de3
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-03-03 21:23:14 +0500

    slug convertor product

--- goods/views.py
+++ goods/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render
 from goods.models import Products
 
 
+# каталог
 def catalog(request):
-
     goods = Products.objects.all()
-
 
     context={
         "title": "Home - Каталог",
@@ -15,6 +14,11 @@ def catalog(request):
     return render(request,"goods/catalog.html", context)
 
 
-def product(request):
+# ссылка на продукты url по слагу
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
 
-    return render(request, "goods/product.html")
+    context = {
+        'product': product,
+    }
+    return render(request, "goods/product.html", context=context)
