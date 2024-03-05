8533f66da2d52ad4fd3667e0cc0cafbf19116a13
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-03-05 22:18:02 +0500

    the product filter on the site page and urlencode

--- goods/views.py
+++ goods/views.py
@@ -7,17 +7,24 @@ from goods.models import Products
 # каталог  все товары и тд 
 def catalog(request, category_slug):
 
+# пагинация и фильр товара на главной странице
     page = request.GET.get('page', 1)
-
-
-
-
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-        
+    
+# фильтр сортировки товара по убыванию и возрастанию и по скидке на главной странице
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+
+    
     paginator = Paginator(goods, 3) # пагинатор количество элементов на одной странице
     current_page = paginator.page(int(page))
 
