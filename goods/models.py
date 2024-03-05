4c9f83b5ae9aa34557951aeacea9aa6964856970
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-03-05 19:08:27 +0500

    the first paginations options

--- goods/models.py
+++ goods/models.py
@@ -31,12 +31,13 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        ordering = ('id', )
 
 
     def __str__(self):
         return f' {self.name} Количество - {self.quantity}' 
     
-
+# id: 00001
     def display_id(self):
         return f'{self.id:05}'
     
