ee9d0bec413258cdeae99eda1e36eb42cda255b4
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-03-22 14:55:08 +0500

    customs validators

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,10 @@
+from ctypes import resize
+import re
+from typing import Any
 from django import forms
 
-
+# форма для оформленния заказа
 class CreateOrderForm(forms.Form):
-
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
@@ -13,6 +15,20 @@ class CreateOrderForm(forms.Form):
     payment_on_get = forms.ChoiceField(
         choices=[("0", "False"), ("1", "True")],
     )
+
+
+# проверка на правильный ввод номер телефона
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError('Номер телефона должен содержать только цифры')
+        
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError('Неверный формат номера')
+        
+        return data
 
 
 #     first_name = forms.CharField(
