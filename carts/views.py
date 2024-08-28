7cedec543b550ee6129390054513f741f807c294
agliullin_a_f <agliullin.artem13@mail.ru>
2024-08-28 15:43:29 +0300

    (v.1.2.2) update all View

--- carts/views.py
+++ carts/views.py
@@ -4,102 +4,177 @@ from django.contrib import sessions
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
+from django.views import View
 
+from carts.mixins import CartMixin
 from carts.utils import get_user_carts
 from goods.models import Products
 from carts.models import Cart
 
 
-# корзина для пользоватля добавления
-def cart_add(request):
+
+# корзина для пользоватля добавлени
     
-    product_id = request.POST.get('product_id')
+class CartAddView(CartMixin, View):
+    def post(self, request):
+        product_id = request.POST.get('product_id')
+        product = Products.objects.get(id=product_id)
 
-    product = Products.objects.get(id=product_id)
+        cart = self.get_cart(request, product=product)
 
-    if request.user.is_authenticated:
-        carts = Cart.objects.filter(user=request.user, product=product)
-
-
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity +=1
-                cart.save()
+        if cart:
+            cart.quantity += 1
+            cart.save()
         else:
-            Cart.objects.create(user=request.user, product=product, quantity=1)
-            
-    else:
-        carts = Cart.objects.filter(
-            session_key=request.session.session_key, product=product)
+            Cart.objects.create(user=request.user if request.user.is_authenticated else None,
+                                session_key=request.session.sesiion_key if not request.user.is_authenticated else None,
+                                product=product, quantity=1)
+
+        response_data = {
+            'message': 'Товар добален в корзину',
+            'cart_items_html': self.render_cart(request)
+        }
+        return JsonResponse(response_data)
+
+
+
+class CartChangeView(CartMixin, View):
+    def post(self, request):
+        cart_id = request.POST.get('cart_id')
+
+        cart = self.get_cart(request, cart_id=cart_id)
+
+        cart.quantity = request.POST.get('quantity')
+        cart.save()
+
+        quantity = cart.quantity
+
+
+
+        response_data = {
+            'message': 'Количество изменено',
+            'quantity': quantity,
+            'cart_items_html': self.render_cart(request)
+        }
+        return JsonResponse(response_data)
+
+
+
+class CartRemoveView(View, CartMixin):
+    def post(self, request):
+        cart_id = request.POST.get('cart_id')
+
+        cart = self.get_cart(request, cart_id=cart_id)
+        qyantity = cart.quantity
         
-        if carts.exists():
-            cart = carts.first()
-            if cart:
-                cart.quantity +=1
-                cart.save()
+        cart.delete()
 
-        else:
-            Cart.objects.create(
-                session_key=request.session.session_key, product=product, quantity=1
-            )
+
+        response_data = {
+            'message': 'Товар удален',
+            'cart_items_html': self.render_cart(request),
+            'quantity_deleted': qyantity
+        }
+        return JsonResponse(response_data)
+
+
+
+        # product_id = request.POST.get('product_id')
+
+        # product = Products.objects.get(id=product_id)
+
+        # if request.user.is_authenticated:
+        #     carts = Cart.objects.filter(user=request.user, product=product)
+
+
+        #     if carts.exists():
+        #         cart = carts.first()
+        #         if cart:
+        #             cart.quantity +=1
+        #             cart.save()
+        #     else:
+        #         Cart.objects.create(user=request.user, product=product, quantity=1)
+                
+        # else:
+        #     carts = Cart.objects.filter(
+        #         session_key=request.session.session_key, product=product)
+            
+        #     if carts.exists():
+        #         cart = carts.first()
+        #         if cart:
+        #             cart.quantity +=1
+        #             cart.save()
+
+        #     else:
+        #         Cart.objects.create(
+        #             session_key=request.session.session_key, product=product, quantity=1
+        #         )
+        
+        # user_cart = get_user_carts(request)
+        # cart_items_html = render_to_string(
+        #     'carts/includes/included_cart.html', {'cart': user_cart}, request=request)
+
+        # response_data = {
+        #     'message': 'Товар добавлен в корзину',
+        #     'cart_items_html': cart_items_html,
+        # }
+        # return JsonResponse(response_data)
+
+
+
+
+
+
+# # изменение карзины
+# def cart_change(request):
+
+#     cart_id = request.POST.get('cart_id')
+#     quantity = request.POST.get('quantity')
+
+#     cart = Cart.objects.get(id=cart_id)
+
+#     cart.quantity = quantity
+#     cart.save()
+
+#     cart = get_user_carts(request)
+#     cart_items_html = render_to_string(
+#         'carts/includes/included_cart.html', {'carts': cart}, request=request)
     
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'cart': user_cart}, request=request)
+#     response_data = {
+#         'message': 'Количество изменено',
+#         'cart_items_html': cart_items_html,
+#         'quantity': quantity
+#     }
 
-    response_data = {
-        'message': 'Товар добавлен в корзину',
-        'cart_items_html': cart_items_html,
-    }
-    return JsonResponse(response_data)
-
-
-# изменение карзины
-def cart_change(request):
-
-    cart_id = request.POST.get('cart_id')
-    quantity = request.POST.get('quantity')
-
-    cart = Cart.objects.get(id=cart_id)
-
-    cart.quantity = quantity
-    cart.save()
-
-    cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': cart}, request=request)
+#     return JsonResponse(response_data)
     
-    response_data = {
-        'message': 'Количество изменено',
-        'cart_items_html': cart_items_html,
-        'quantity': quantity
-    }
 
-    return JsonResponse(response_data)
-    
+
+
+
+
+
 
 # для удаления продуктов из корзины пользователя
-def cart_remove(request):
-     
-    cart_id = request.POST.get('cart_id')
+# def cart_remove(request):
+#     cart_id = request.POST.get('cart_id')
 
-    cart = Cart.objects.get(id=cart_id)
-    quantity = cart.quantity
-    cart.delete()
+#     cart = Cart.objects.get(id=cart_id)
+#     quantity = cart.quantity
+#     cart.delete()
 
 
-    user_cart = get_user_carts(request)
-    cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request)
+#     user_cart = get_user_carts(request)
+#     cart_items_html = render_to_string(
+#         'carts/includes/included_cart.html', {'carts': user_cart}, request=request)
     
-    response_data = {
-        'message': 'Товар удален',
-        'cart_items_html': cart_items_html,
-        'quantity_deleted': quantity
-    }
+#     response_data = {
+#         'message': 'Товар удален',
+#         'cart_items_html': cart_items_html,
+#         'quantity_deleted': quantity
+#     }
 
-    return JsonResponse(response_data)
+#     return JsonResponse(response_data)
 
 
     # cart = Cart.objects.get(id=cart_id)
--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,6 @@ app_name = 'orders'
 
 
 urlpatterns = [
-    path('create-order/', views.create_order, name='create_order'),
+    path('create-order/', views.CreateOrderView.as_view(), name='create_order'),
     
 ]
